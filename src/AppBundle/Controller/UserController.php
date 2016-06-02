3b34a6dca8782c40f43ed45e323063b231e93788
Milos Tomic <tmilos@gmail.com>
2016-06-02 15:39:38 +0200

    user su

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -2,9 +2,12 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Model\Account;
+use Appsco\Dashboard\ApiBundle\Security\Core\Authentication\Token\AppscoToken;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends Controller
 {
@@ -20,9 +23,29 @@ class UserController extends Controller
         foreach ($this->get('user_store')->listUsers() as $username) {
             $users[$username] = $this->get('store')->load($username);
         }
-        
+
         return [
             'users' => $users,
         ];
+    }
+
+    /**
+     * @param $username
+     *
+     * @return Response
+     *
+     * @Route("user/su/{username}", name="user.su")
+     */
+    public function suAction($username)
+    {
+        $translations = $this->get('store')->load($username);
+        $account = new Account($username, $translations);
+        $appscoUser = new \Appsco\Dashboard\ApiBundle\Model\Account();
+        $appscoUser->setEmail($username);
+
+        $token = new AppscoToken($account, $account->getRoles(), $appscoUser, null, null);
+        $this->get('security.token_storage')->setToken($token);
+
+        return $this->redirectToRoute('project.list');
     }
 }
